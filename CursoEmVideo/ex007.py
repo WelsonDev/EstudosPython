f7812978a36d2e9270755e614495df01574fc619
Welson Santos <welsonprofissional@gmail.com>
2023-02-18 22:04:59 -0300

    Realizado ex 7

--- CursoEmVideo/ex007.py
+++ CursoEmVideo/ex007.py
@@ -2,6 +2,8 @@
 desenvolva um programa que duas notas do
 aluno e calcule sua media
 """
-nota_1 = int(input("Digite a primeira nota:\n"))
-nota_2 = int(input("Digite a segunda nota:\n"))
+nota_1 = float(input("Digite a primeira nota:\n"))
+nota_2 = float(input("Digite a segunda nota:\n"))
+media = (nota_1 + nota_2) / 2
 
+print("A Sua media e igual a {}".format(media))
