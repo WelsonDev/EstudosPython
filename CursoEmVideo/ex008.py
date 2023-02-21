d6e50beff4e6dbff107ac7249e5f4a05e707f0b5
Welson Santos <welsonprofissional@gmail.com>
2023-02-20 22:58:59 -0300

    Realizado ex 8

---
+++ CursoEmVideo/ex008.py
@@ -0,0 +1,11 @@
+"""
+escreva um programa que leia um valor
+em metros e o exiba convertido em
+centimetros e milimetros
+"""
+
+metro = float(input('Digite o valor em metros: \n'))
+centimetro = metro / 100
+milimetro = metro / 1000
+
+print('o Valor digitado em Centimetros e: {} e em milimetros e: {}'.format(centimetro, milimetro))
