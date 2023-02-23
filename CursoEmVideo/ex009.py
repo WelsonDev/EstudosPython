c05518c7c7cf625b24e75d4cf63e520cb1da5d04
WelsonWorkPC <Welsonprofissional@gmail.com>
2023-02-23 17:03:15 -0300

    Ex 09

--- CursoEmVideo/ex009.py
+++ CursoEmVideo/ex009.py
@@ -3,3 +3,13 @@ faca um programa que leia um numero qualque
 e mostre sua tabuada
 """
 
+number = int(input('Digite um nemero e veja a tabuada:\n'))
+numerador = 1
+resultado = number + numerador
+
+for numerador in range(10):
+    if(numerador < 10):
+        numerador += 1
+    print("{} x {} = {}" .format(number, numerador, resultado))
+else:
+    print('Fim')
