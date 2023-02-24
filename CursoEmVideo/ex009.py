cc57423bd6e4461f15c1e0a281dc338174b3b2e1
Welson Santos <welsonprofissional@gmail.com>
2023-02-23 23:12:06 -0300

    Realizado ex 9

--- CursoEmVideo/ex009.py
+++ CursoEmVideo/ex009.py
@@ -5,11 +5,12 @@ e mostre sua tabuada
 
 number = int(input('Digite um nemero e veja a tabuada:\n'))
 numerador = 1
-resultado = number + numerador
+# resultado = number + numerador
 
 for numerador in range(10):
-    if(numerador < 10):
+    if numerador < 10:
         numerador += 1
+        resultado = number * numerador
     print("{} x {} = {}" .format(number, numerador, resultado))
 else:
     print('Fim')
