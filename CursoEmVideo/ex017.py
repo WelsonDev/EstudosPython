595f3e3709b2a8b924009db4dfb43ac39c0903ab
Welson Santos <welsonprofissional@gmail.com>
2023-03-01 23:22:49 -0300

    realizado ex 17

--- CursoEmVideo/ex017.py
+++ CursoEmVideo/ex017.py
@@ -4,6 +4,13 @@ do cateto adjacente de um triangulo
 retangulo, calcule e mostre o
 comprimento da hipotenusa
 """
+import math
 
-cat = input('Digite o valor do cateto adjacente')
+cata = float(input('Digite o valor do cateto adjacente'))
+cato = float(input('Digite o valor do cateto oposto'))
+
+hipo = (cata ** 2 + cato ** 2) ** 4 (1/2)
+hipo2 = math.hypot(cato, cata)
+print('{}'.format(hipo))
+print('{}'.format(hipo2))
 
