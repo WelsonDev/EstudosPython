405d0a4d35aa7baa1305f55674e6948debc7a29c
Welson Santos <welsonprofissional@gmail.com>
2023-03-11 21:53:30 -0300

    exercicio 22 completo

--- CursoEmVideo/ex022.py
+++ CursoEmVideo/ex022.py
@@ -8,5 +8,5 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 nome = str(input('Digite seu nome'))
 
 print('Esse e seu nome em maiusculo, {}'.format(nome.upper()))
-print('Seu nome possui {} letras'.format(len(nome)))
+print('Seu nome possui {} letras'.format(len(nome.strip())))
 print('Seu primeiro nome possui {} letras'.format(int(nome.find(" "))))
