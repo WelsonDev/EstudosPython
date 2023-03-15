47b299c8be638519b635c5bbcc679f7d87dd7a93
Welson Santos <welsonprofissional@gmail.com>
2023-03-14 23:06:53 -0300

    exercicio 27 completo

--- CursoEmVideo/ex027.py
+++ CursoEmVideo/ex027.py
@@ -2,3 +2,10 @@
 Faça um programa que leia o nome completo de uma pessoa,
 mostrando em seguida o primeiro e o último nome separadamente.
 """
+nome = str(input('Digite seu nome'))
+nome = nome.split()
+
+cont = len(nome)
+
+print('Seu primeiro nome e: {}'.format(nome[0]))
+print('Seu ultimo nome e: {}'.format(nome[-1]))
