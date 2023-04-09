608131bfb2915811b54eb32e642cef2ce4ffb975
Welson Santos <welsonprofissional@gmail.com>
2023-04-08 21:13:02 -0300

    Exercicio 29 completo

--- CursoEmVideo/ex029.py
+++ CursoEmVideo/ex029.py
@@ -2,3 +2,12 @@
 Exercício Python 029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
 mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
 """
+
+velociade = int(input('Digite a velociade que voce esta dirigindo:\n'))
+
+multa = (velociade - 80) * 7
+
+if velociade > 80:
+    print('Voce esta acima da velociade permitida, Sua multa e de R$ {:.2f}'.format(multa))
+else:
+    print('Voce esta dentro do limite de velociade')
