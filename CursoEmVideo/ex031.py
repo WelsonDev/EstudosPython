44ba355ba3a46eb8230acefd6b163b8f3e3cd329
Welson Santos <welsonprofissional@gmail.com>
2023-04-09 17:40:25 -0300

    Exercicio 31 completo

--- CursoEmVideo/ex031.py
+++ CursoEmVideo/ex031.py
@@ -4,3 +4,10 @@ Exercício Python 031: Desenvolva um programa que pergunte a distância de uma v
 cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
 
 """
+
+distancia = int(input('Digite a distancia em kilometros da sua viagem\n'))
+
+if distancia <= 200:
+    print('O valor da passagem e R$ {:.2f}'.format(distancia * 0.50))
+else:
+    print('O valor da passagem e R$ {:.2f}'.format(distancia * 0.45))
