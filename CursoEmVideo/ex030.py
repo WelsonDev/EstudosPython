fa3b4bbebda12d8a0606c30dbcac74d50f571f42
Welson Santos <welsonprofissional@gmail.com>
2023-04-08 21:26:00 -0300

    Exercicio 30 completo

--- CursoEmVideo/ex030.py
+++ CursoEmVideo/ex030.py
@@ -1,3 +1,10 @@
 """"
 Exercício Python 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 """
+
+numero = int(input('Digite um numero para saber se e par ou impar:\n'))
+
+if numero % 2 == 0:
+    print('O numero digitado {} e par'.format(numero))
+else:
+    print('O Numero digitado {} e impar'.format(numero))
