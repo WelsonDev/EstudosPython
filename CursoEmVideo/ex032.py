452742a5ee4b0e1766a1333893b378ce128bf4c7
Welson Santos <welsonprofissional@gmail.com>
2023-04-09 21:33:14 -0300

    Exercicio 32 completo

--- CursoEmVideo/ex032.py
+++ CursoEmVideo/ex032.py
@@ -1,5 +1,11 @@
 """"
 
 Exercício Python 032: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
-
 """
+
+ano = int(input('Digite o ano para verificar se e bissexto'))
+
+if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+    print('Ano bissexto.')
+else:
+    print('Não é um ano bissexto.')
