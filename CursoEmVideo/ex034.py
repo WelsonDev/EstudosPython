a7ebc3ccc4756204f2b2a0c8f36d4a88e3090b1d
Welson Santos <welsonprofissional@gmail.com>
2023-04-14 23:23:51 -0300

    Exercicio 34

--- CursoEmVideo/ex034.py
+++ CursoEmVideo/ex034.py
@@ -3,3 +3,10 @@ Exercício Python 034: Escreva um programa que pergunte o salário de um funcion
 Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
 
 """
+
+salario = float(input("Digite o seu salario:\n"))
+
+if salario < 1250:
+    print("Seu salario com aumento e: {}".format(salario + 10))
+else:
+    print("Seu salario com aumento e: {}".format(salario * 15 / 100))
