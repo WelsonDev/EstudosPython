cdc0c102f984ee004ab6b6003aeb4939f69a87a1
Welson Santos <welsonprofissional@gmail.com>
2023-04-14 22:33:10 -0300

    Exercicio 33

--- CursoEmVideo/ex033.py
+++ CursoEmVideo/ex033.py
@@ -2,3 +2,9 @@
 Exercício Python 033: Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
 """
+
+n1 = int(input("Digite o primeiro numero"))
+n2 = int(input("Digite o segundo numero"))
+n3 = int(input("Digite o terceiro numero"))
+
+
