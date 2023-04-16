177cc37a387eb91cfcd6787bfd30f2641b859ea0
Welson Santos <welsonprofissional@gmail.com>
2023-04-16 20:48:17 -0300

    Exercicio 33 feito

--- CursoEmVideo/ex033.py
+++ CursoEmVideo/ex033.py
@@ -6,5 +6,28 @@ Exercício Python 033: Faça um programa que leia três números e mostre qual �
 n1 = int(input("Digite o primeiro numero"))
 n2 = int(input("Digite o segundo numero"))
 n3 = int(input("Digite o terceiro numero"))
+""""
+# Verifica qual numero e menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+# Verifica qual numero e maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    menor = n2
+if n3 > n1 and n3 > n2:
+    menor = n3
+    
+print("O menor numero digitado foi: {}".format(menor))
+print("O maior numero digitado foi: {}".format(maior))
+
+"""
+lista = [n1, n2, n3]
+lista_ordenada = sorted(lista)
+
+print('O menor número é {}'.format(lista_ordenada[0]))
+print('O maior número é {}'.format(lista_ordenada[2]))
 
 
