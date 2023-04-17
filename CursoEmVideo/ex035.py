2fc104ed37d6382e24ad33aef1b29188aef1208f
Welson Santos <welsonprofissional@gmail.com>
2023-04-16 21:04:02 -0300

    Exercicio 35 feito

--- CursoEmVideo/ex035.py
+++ CursoEmVideo/ex035.py
@@ -3,3 +3,13 @@ Exercício Python 035: Desenvolva um programa que leia o comprimento de três re
 e diga ao usuário se elas podem ou não formar um triângulo.
 
 """
+print("=" * 20)
+
+n1 = float(input("Digite o primeiro segmento: "))
+n2 = float(input("Digite o segundo segmento: "))
+n3 = float(input("Digite o terciro segmento: "))
+
+if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
+    print("Os Segmentos acima podem formar um triangulo: ")
+else:
+    print("Os segmetos acima nao podem formar um triangulo")
