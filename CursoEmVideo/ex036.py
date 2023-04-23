ed2e2820a1b3e6efa2c8804198833603cff3d069
Welson Santos <welsonprofissional@gmail.com>
2023-04-23 18:42:31 -0300

    Ex. 36 completo

--- CursoEmVideo/ex036.py
+++ CursoEmVideo/ex036.py
@@ -5,3 +5,22 @@ ou então o empréstimo será negado.
 
 
 """
+print("\033[30m=\033[m"*30)
+print("Bem vindo ao financiamento python")
+print("\033[30m=\033[m"*20)
+
+vlcasa = float(input("Digite o valor da casa que voce quer comprar:\n"))
+tpagamento = int(input("Em quanto tempo voce quer pagar o seu financiamento?\n")) * 12
+vlsalario = float(input("Digite o valor do seu salario:\n"))
+compsalario = vlsalario * 30 / 100
+vparcelea = vlcasa / tpagamento
+print("\033[30m=\033[m"*20)
+
+# print("O Valor da parcela sera: {:.2f} \n".format(compsalario))
+
+if vparcelea > compsalario:
+    print("O valor da parcela e maior que 30% do seu salario, financiamento negado")
+else:
+    print("O valor da parcela e menor que 30% do seu salario, financiamento aprovado")
+
+print("\033[30m=\033[m"*20)
