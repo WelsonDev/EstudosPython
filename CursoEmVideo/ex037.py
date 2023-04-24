e1b7d4ed75fd4596763bac15fec89a30ca30f4f0
Welson Santos <welsonprofissional@gmail.com>
2023-04-23 22:44:01 -0300

    Ex. 37 completo

--- CursoEmVideo/ex037.py
+++ CursoEmVideo/ex037.py
@@ -2,4 +2,17 @@
 Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base
 de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
 """
+print("\033[30m=\033[m"*30)
 
+escolha = int(input("Escolha a base que voce quer converter:\n 1: Binario\n 2: octal\n 3: hexadecinal\n"))
+
+valor = int(input("Digite o valor que voce quer converter:\n"))
+
+if escolha == 1:
+    print("O valor digitado foi: {}, A conversao para binario e: {}".format(escolha, bin(valor)))
+elif escolha == 2:
+    print("O valor digitado foi: {}, A conversao para octal e: {}".format(escolha, oct(valor)))
+elif escolha == 3:
+    print("O valor digitado foi: {}, A conversao para hexadecimal e: {}".format(escolha, hex(valor)))
+else:
+    print("Opcao invalida!")
