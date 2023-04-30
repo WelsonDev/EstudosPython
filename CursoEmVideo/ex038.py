b4deba232ef992522abd850c53d2c3242181b3da
Welson Santos <welsonprofissional@gmail.com>
2023-04-29 23:24:34 -0300

    Ex. 38 completo

--- CursoEmVideo/ex038.py
+++ CursoEmVideo/ex038.py
@@ -4,3 +4,13 @@ Escreva um programa que leia dois números inteiros e compare-os. mostrando na t
 - O segundo valor é maior
 - Não existe valor maior, os dois são iguais
 """
+
+prinum = int(input("Digite o valor:\n"))
+segnum = int(input("Digite o segundo valor\n"))
+
+if prinum > segnum:
+    print("O primeiro valor digitado: {}, e maior que o segundo valor digitado: {}".format(prinum, segnum))
+elif segnum > prinum:
+    print("O segundo valor digitado: {}, e maior que o primeiro valor digitado: {}".format(segnum, prinum))
+else:
+    print("Nao existe valor maior, os dois sao iguais!")
