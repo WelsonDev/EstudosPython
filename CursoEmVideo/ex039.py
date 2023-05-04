e4f61ab7b83874484741affe011d870e50f1ef80
Welson Santos <welsonprofissional@gmail.com>
2023-05-03 21:07:24 -0300

    Ex. 39 andamento

--- CursoEmVideo/ex039.py
+++ CursoEmVideo/ex039.py
@@ -3,11 +3,16 @@ Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo 
 se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou
 do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 """
-import datetime as tm
+import datetime as dt
+import time as tm
+anonasc = int(input("Digite o ano que voce nasceu:\n"))
 
-#anonasc = int(input("Digite o ano que voce nasceu:\n"))
+anohj = int(tm.strftime("%Y"))
 
-anohj = tm.strftime("%d/%m/%y")
+if anohj == anonasc:
+    print("Ano e igual")
+else:
+    print("Ano na e igual")
 
 print("{}".format(anohj))
 
