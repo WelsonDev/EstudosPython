22f7706e6d47c3087046b5eb34f93f98214a1bf5
Welson Santos <welsonprofissional@gmail.com>
2023-05-13 22:18:03 -0300

    Ex. 41 finalizado

--- CursoEmVideo/ex041.py
+++ CursoEmVideo/ex041.py
@@ -7,3 +7,21 @@ categoria, de acordo com a idade:
 - Até 25 anos: SÊNIOR
 - Acima de 25 anos: MASTER
 """
+import time as tm
+
+idade = int(input("Digite a sua idade:\n"))
+
+anohj = int(tm.strftime("%Y"))
+
+categoria = anohj - idade
+
+if categoria <= 9:
+    print("Sua categoria e MIRIM.\n")
+elif categoria <= 14:
+    print("Sua categoria e INFANTIL.\n")
+elif categoria <= 19:
+    print("Sua categoria e JÚNIOR.\n")
+elif categoria <= 25:
+    print("Sua categoria e SÊNIOR.\n")
+else:
+    print("Sua categoria e MASTER.\n")
