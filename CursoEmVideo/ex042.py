286f2817dddcd48fa54c787ae51991554af72706
Welson Santos <welsonprofissional@gmail.com>
2023-05-22 20:52:15 -0300

    realizando ex: 42

--- CursoEmVideo/ex042.py
+++ CursoEmVideo/ex042.py
@@ -5,3 +5,14 @@
 - ESCALENO: todos os lados diferentes
 """
 
+# print("=\033[0:33:44m" * 20)
+print("=" * 20)
+
+n1 = float(input("Digite o primeiro segmento: "))
+n2 = float(input("Digite o segundo segmento: "))
+n3 = float(input("Digite o terciro segmento: "))
+
+if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
+    print("Os Segmentos acima podem formar um triangulo: ")
+else:
+    print("Os segmetos acima nao podem formar um triangulo")
