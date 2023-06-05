4761a0e82f45c06883d8d45453b7f12e2020a36e
Welson Santos <welsonprofissional@gmail.com>
2023-06-04 21:38:33 -0300

    Exercicio: 44 em andamento

--- CursoEmVideo/ex044.py
+++ CursoEmVideo/ex044.py
@@ -1,8 +1,19 @@
 """"
-044: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+044: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição
+de pagamento:
+
 - à vista dinheiro/cheque: 10% de desconto
 - à vista no cartão: 5% de desconto
 - em até 2x no cartão: preço formal
 - 3x ou mais no cartão: 20% de juros
 
 """
+valorproduto = float(input("Digite o valor do produto:\n"))
+
+formapgto = int(input("1 - A vista em dinheiro:\n" 
+                      "2 - A vista no cartao de credito:\n"
+                      "3 - ate 2x no cartao:\n"
+                      "4 - Maior que 3x no cartao:\n"
+
+                      ))
+print(formapgto)
